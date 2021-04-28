178ab885a5205d97a1f71a1c9ad781bf8e91abde
Danil Rafiqi <danil.rafiqi@nri.co.id>
2021-04-28 16:06:26 +0700

    chore: initial commit

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.8
 import * as React from 'react';
-import { ViewStyle } from 'react-native';
+import type { ViewStyle } from 'react-native';
 
 export interface StepsAsProps {
   index: number;
